e5a20bceaab61c4f3db05a257c535b43ffba0ea9
Mahkii900 <col.graff.battleschool@gmail.com>
2019-07-29 15:52:53 -0600

    second commit

--- src/store.js
+++ src/store.js
@@ -2,20 +2,45 @@ import {createStore} from 'redux'
 
 //INITIAL STATE
 const initialState = {
-    currentValue: 0
+    currentValue: 0,
+    futureValues: [],
+    previousValues: []
 }
 
 //ACTION CONSTS
 export const INCREMENT = 'INCREMENT'
 export const DECREMENT = 'DECREMENT'
+export const UNDO = 'UNDO'
+export const REDO = 'REDO'
 
 //REDUCER
 function countReducer(state = initialState, action) {
     switch(action.type) {
         case INCREMENT:
-            return {...state, currentValue: state.currentValue + action.payload}
+            return {
+                currentValue: state.currentValue + action.payload,
+                futureValues: [],
+                previousValues: [state.currentValue, ...state.previousValues]
+            }
         case DECREMENT:
-            return {...state, currentValue: state.currentValue - action.payload}
+            return {
+                currentValue: state.currentValue - action.payload,
+                futureValues: [],
+                previousValues: [state.currentValue, ...state.previousValues]
+            }
+        case UNDO:
+            var previousVals = state.previousValues.slice(1)
+            return {
+                currentValue: state.previousValues[0],
+                futureValues: [state.currentValue, ...state.futureValues],
+                previousValues: [...previousVals]
+            }
+        case REDO:
+            return {
+                currentValue: state.futureValues[0],
+                futureValues: state.futureValues.slice(1),
+                previousValues: [state.currentValue, ...state.previousValues]
+            }
         default: return state
     }
 }
